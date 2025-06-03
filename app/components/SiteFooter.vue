07f78c9cce244eb30ac39d784f60572bb8b9bcb1
Joker2Yue <Joker2Yue@qq.com>
2025-06-03 15:06:00 +0800

    refactor(components): 更新 SiteFooter组件中的个人链接信息
    - 修改 GitHub 链接为新的仓库地址
    - 更新个人主页链接- 更换邮箱地址

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/Joker2Yue/site-status",
+  home: "https://www.joker2yue.com",
+  email: "mailto:me@joker2yue.com",
 };
 </script>
 
